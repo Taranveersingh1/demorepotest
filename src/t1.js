cac3fb119976dcc42aced57d9dd501a73e2fbaa7
Taran <taranveersingh130@gmail.com>
2024-11-28 17:29:32 +0530

    initial

---
+++ src/t1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const t1 = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default t1
---
+++ src/t2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const t2 = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default t2
